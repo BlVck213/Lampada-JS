504a2588c5fa0936eb4677d10fca358ad6a9a98e
PAULO CESAR RIBEIRO RODRIGUES PONTES <21192922@CFP127ALUNO.LOCAL>
2021-08-24 10:11:55 -0300

    Script e Trabalho Completo

---
+++ lampada.js
@@ -0,0 +1,52 @@
+"use strict"
+
+
+function lampadaQuebrada() {
+ return document.getElementById("lampada").src.indexOf("quebrada") !== -1
+}
+
+function ligarLampada(){
+
+    const lampada = document.getElementById("lampada")
+
+    if(!lampadaQuebrada()){
+    
+    lampada.src = "ligada.jpg"
+    ligar.disabled = true
+    desligar.disabled = false
+    }
+}
+
+function desligarLampada(){
+
+    const lampada = document.getElementById("lampada")
+
+    if(!lampadaQuebrada()){
+        
+    lampada.src = "desligada.jpg"
+    ligar.disabled = false
+    desligar.disabled = true
+    }
+}
+
+function quebrarLampada(){
+    const lampada = document.getElementById("lampada")
+    lampada.src = "quebrada.jpg"
+    ligar.disabled = true
+    desligar.disabled = true
+}
+
+document.getElementById("ligar")
+.addEventListener("click", ligarLampada)
+
+document.getElementById("desligar")
+.addEventListener("click", desligarLampada)
+
+document.getElementById("lampada")
+.addEventListener("dblclick", quebrarLampada)
+
+document.getElementById("lampada")
+.addEventListener("mouseover", ligarLampada)
+
+document.getElementById("lampada")
+.addEventListener("mouseleave", desligarLampada)
