b2b757328b4e77edb60508fe713399019d8aeb9c
BlVck213 <21192922@CFP127ALUNO.LOCAL>
2021-08-31 09:19:27 -0300

    Novas Ações Adicionadas

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Site criado com objetivos didáticos para as aulas de PWFE do curso de Desenvolvimento de Sistemas do [SENAI Jandira](https://jandira.sp.senai.br/).
 
-O projeto consiste em criar uma lâmpada que tenha tenha três ações, ligar, desligar e quebrar, utilizando vários eventos.  
+O projeto consiste em criar uma lâmpada que tenha cinco ações, ligar, desligar, piscar, parar e quebrar, utilizando vários eventos.  
 
 ![](img/ligada.jpg) ![](img/desligada.jpg) ![](img/quebrada.jpg)
 
--- lampada.js
+++ lampada.js
@@ -1,16 +1,22 @@
 "use strict"
 
+const lampada = document.getElementById("lampada")
+let idInterval
 
-function lampadaQuebrada() {
+/* function lampadaQuebrada() {
  return document.getElementById("lampada").src.indexOf("quebrada") !== -1
-}
+} */
+
+const lampadaQuebrada = () => lampada.src.indexOf("quebrada") !== -1
+
+/* function lampadaDesligada(){
+    return lampada.src.includes("desligada")
+} */
+
+const lampadaDesligada = () => lampada.src.includes("desligada")
 
 function ligarLampada(){
-
-    const lampada = document.getElementById("lampada")
-
     if(!lampadaQuebrada()){
-    
     lampada.src = "img/ligada.jpg"
     ligar.disabled = true
     desligar.disabled = false
@@ -18,11 +24,7 @@ function ligarLampada(){
 }
 
 function desligarLampada(){
-
-    const lampada = document.getElementById("lampada")
-
-    if(!lampadaQuebrada()){
-        
+    if(!lampadaQuebrada()){ 
     lampada.src = "img/desligada.jpg"
     ligar.disabled = false
     desligar.disabled = true
@@ -30,11 +32,31 @@ function desligarLampada(){
 }
 
 function quebrarLampada(){
-    const lampada = document.getElementById("lampada")
     lampada.src = "img/quebrada.jpg"
     ligar.disabled = true
     desligar.disabled = true
+    piscar.disabled = true
 }
+
+function trocarLampada(){
+    if(lampadaDesligada()){
+        ligarLampada()
+    } else {
+         desligarLampada()
+    }
+}
+
+function piscarLampada(){
+    const piscar = document.getElementById("piscar")
+    if(piscar.textContent == "Piscar"){
+        idInterval = setInterval(trocarLampada, 750)  
+         piscar.textContent = "Parar"
+    } else {
+        clearInterval(idInterval)
+        piscar.textContent = "Piscar"
+    }
+}
+
 
 document.getElementById("ligar")
 .addEventListener("click", ligarLampada)
@@ -50,3 +72,21 @@ document.getElementById("lampada")
 
 document.getElementById("lampada")
 .addEventListener("mouseleave", desligarLampada)
+
+document.getElementById("piscar")
+.addEventListener("click", piscarLampada)
+
+
+
+/*const some = function (a, b){
+    return a + b
+}
+const soma = (a, b) => {
+    return a + b
+}
+const somar = (a, b) => a + b
+
+const soma10 = function (a){
+    return a + 10
+}
+const soma1O = (a) => a + 10 */
